d05d04ea88db1bebb0b2505f3793cee5f87b58c0
Cody <2720299091@qq.com>
2022-05-24 23:30:11 +0800

    commit

--- standard/standard.go
+++ standard/standard.go
@@ -47,11 +47,9 @@ func rs() (s string) {
 func scan(a ...interface{}) { Fscan(in, a...) }
 
 //print 用法与 fmt.print() 相似，可以传入多个变量进行输出。
-//注意，这个函数不会换行，多个变量之间有空格
 func print(a ...interface{}) { Fprint(out, a...) }
 
-//println 用法与 fmt.println() 相似，可以传入多个变量进行输出。
-//这个函数会换行，多个变量之间有空格
+//println 用法与 fmt.println() 相似，可以传入多个变量进行输出并在最后换行。
 func println(a ...interface{}) { Fprintln(out, a...) }
 
 //min 可以传入多个 int 变量，求出这些变量的最小值
@@ -99,6 +97,7 @@ func max64(a ...int64) int64 {
 }
 
 //sum 可以传入多个 int 变量，求出这些变量的总和，并返回总和。
+//
 //你可以使用 ... 运算符打散数组，使得 sum 函数可以求出数组的和。
 func sum(a ...int) int {
 	ans := 0
@@ -109,7 +108,8 @@ func sum(a ...int) int {
 }
 
 //sum64 可以传入多个 int64 变量，求出这些变量的总和，并返回总和。
-//你可以使用 ... 运算符打散数组，使得 sum 函数可以求出数组的和。
+//
+//你可以使用 ... 运算符打散数组，使得 sum64 函数可以求出数组的和。
 func sum64(a ...int64) int64 {
 	ans := int64(0)
 	for _, x := range a {
@@ -132,9 +132,10 @@ func reverse64(a []int64) {
 	}
 }
 
-//lowerBound 类似于 C++ 中的 lower_bound。
-//用于二分查找 int 切片中，第一个大于等于 k 的元素在切片中的位置。
+// lowerBound 类似于 C++ 中的 lower_bound，用于二分查找 int 切片中，第一个大于等于 k 的元素在切片中的位置。
+//
 //如果元素不存在，则返回切片的长度。
+//
 //注意，lowerBound 作用的 int 切片必须升序。
 func lowerBound(a []int, k int) int {
 	l, r, pos := 0, len(a)-1, len(a)
@@ -149,10 +150,11 @@ func lowerBound(a []int, k int) int {
 	return pos
 }
 
-//lowerBound64 类似于 C++ 中的 lower_bound。
-//用于二分查找 int64 切片中，第一个大于等于 k 的元素在切片中的位置。
+//lowerBound64 类似于 C++ 中的 lower_bound，用于二分查找 int64 切片中，第一个大于等于 k 的元素在切片中的位置。
+//
 //如果元素不存在，则返回切片的长度。
-//注意，lowerBound 作用的 int64 切片必须升序。
+//
+//注意，lowerBound64 作用的 int64 切片必须升序。
 func lowerBound64(a []int64, k int64) int {
 	l, r, pos := 0, len(a)-1, len(a)
 	for l <= r {
@@ -166,9 +168,10 @@ func lowerBound64(a []int64, k int64) int {
 	return pos
 }
 
-//upperBound 类似于 C++ 中的 upper_bound。
-//用于二分查找 int 切片中，第一个大于 k 的元素在切片中的位置。
+//upperBound 类似于 C++ 中的 upper_bound，用于二分查找 int 切片中，第一个大于 k 的元素在切片中的位置。
+//
 //如果元素不存在，则返回切片的长度。
+//
 //注意，upperBound 作用的 int 切片必须升序。
 func upperBound(a []int, k int) int {
 	l, r, pos := 0, len(a)-1, len(a)
@@ -183,10 +186,11 @@ func upperBound(a []int, k int) int {
 	return pos
 }
 
-//upperBound64 类似于 C++ 中的 upper_bound。
-//用于二分查找 int64 切片中，第一个大于 k 的元素在切片中的位置。
+//upperBound64 类似于 C++ 中的 upper_bound，用于二分查找 int64 切片中，第一个大于 k 的元素在切片中的位置。
+//
 //如果元素不存在，则返回切片的长度。
-//注意，upperBound 作用的 int64 切片必须升序。
+//
+//注意，upperBound64 作用的 int64 切片必须升序。
 func upperBound64(a []int64, k int64) int {
 	l, r, pos := 0, len(a)-1, len(a)
 	for l <= r {
@@ -200,6 +204,11 @@ func upperBound64(a []int64, k int64) int {
 	return pos
 }
 
+//unique 类似于 C++ 中的 unique，用于给切片去重。
+//
+//区别在于: unique 函数接受一个 int 切片，并返回一个新的、去好重的切片
+//
+//注意: 传入的切片必须升序
 func unique(a []int) []int {
 	var b []int
 	b = append(b, a[0])
@@ -211,29 +220,41 @@ func unique(a []int) []int {
 	return b
 }
 
+//valSet 将传入切片的所有元素，统一赋值为传入的元素 k
 func valSet(a []int, k int) {
 	for i, _ := range a {
 		a[i] = k
 	}
 }
 
+//Stack 类型是一个 int 类型的栈
+//
+//如果有必要，可以直接通过成员 s（int 切片）遍历栈（s[0] 为栈底）
 type Stack struct {
 	s []int
 }
 
-func (st *Stack) top() int   { return st.s[len(st.s)-1] }
-func (st *Stack) size() int  { return len(st.s) }
-func (st *Stack) pop()       { st.s = st.s[:len(st.s)-1] }
+//top 获取栈的第一个元素
+func (st *Stack) top() int { return st.s[len(st.s)-1] }
+
+//size 返回栈内的元素个数
+func (st *Stack) size() int { return len(st.s) }
+
+//empty 返回一个 bool 类型，表示栈是否为空
+func (st *Stack) empty() bool { return st.size() == 0 }
+
+//pop 弹出栈顶元素
+func (st *Stack) pop() { st.s = st.s[:len(st.s)-1] }
+
+//push 将一个 int 类型从栈顶添加入栈
 func (st *Stack) push(x int) { st.s = append(st.s, x) }
 
+//Queue 类型是一个 int 类型的队列
+//
+//
 type Queue struct {
-	que chan int
-}
-
-func MakeQueue(size int) Queue {
-	var q Queue
-	q.que = make(chan int, size)
-	return q
+	que  []int
+	head int
 }
 
 type PriorityQueue struct {
